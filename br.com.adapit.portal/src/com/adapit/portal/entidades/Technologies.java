ea435d2457774dc52d1d7de78d89c7dc8ea1d9f2
tmelo387 <tmelo387@gmail.com>
2022-05-28 16:26:24 -0300

    remoção de .svn + formatação de texto

--- br.com.adapit.portal/src/com/adapit/portal/entidades/Technologies.java
+++ br.com.adapit.portal/src/com/adapit/portal/entidades/Technologies.java
@@ -1,29 +1,29 @@
 package com.adapit.portal.entidades;
 
 public enum Technologies {
-	Linguagem_de_Programação_Java, 
-	Linguagem_de_Programação_Ponto_Net, 
-	Linguagem_de_Programação_PHP,
-	Linguagem_de_Programação_C_CPlus,
+	Linguagem_de_Programaï¿½ï¿½o_Java, 
+	Linguagem_de_Programaï¿½ï¿½o_Ponto_Net, 
+	Linguagem_de_Programaï¿½ï¿½o_PHP,
+	Linguagem_de_Programaï¿½ï¿½o_C_CPlus,
 	Framework_Ruby_On_Rails,
 	
 	Banco_de_Dados_Oracle,
 	Banco_de_Dados_PostgreSql,
 	
-	Linguagem_de_Marcação,
+	Linguagem_de_Marcaï¿½ï¿½o,
 	Desenvolvimento_de_Sistemas_Ajax,
-	Ferramentas_Produção_WEB,
+	Ferramentas_Produï¿½ï¿½o_WEB,
 	
-	Modelagem_Análise_de_Banco_de_Dados,
-	Modelagem_Análise_de_Sistemas,
-	Modelagem_Ágil_de_Sistemas,
-	Modelagem_de_Processos_de_Negócios,	
+	Modelagem_Analise_de_Banco_de_Dados,
+	Modelagem_Analise_de_Sistemas,
+	Modelagem_Agil_de_Sistemas,
+	Modelagem_de_Processos_de_Negocios,	
 	Desenvolvimento_Dirigido_por_Modelos,
 	Projeto_de_Software,	
-	Técnincas_de_Produção_de_Software,
+	Tecnicas_de_Producao_de_Software,
 	
 	Processo_Desenvolvimento_de_Software,
-	Metodologias_Desenvolvimento_Ágeis,
+	Metodologias_Desenvolvimento_Ageis,
 	Processos_de_Teste_de_Software,
 	Qualidade_de_Software
 }
